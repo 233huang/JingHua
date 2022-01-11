ba559351f98aa5f98a39439064c6b05cbf65eb8f
233huang <1900675683@qq.com>
2022-01-11 17:39:31 +0800

    完成了配表功能

---
+++ Assets/Framework/ConfigTable.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Text;
+
+public class BaseDatebase
+{
+    public int ID;
+}
+
+public class ConfigTable<TDatebase,T> : Singleton<T> 
+    where TDatebase:BaseDatebase,new()
+    where T:new()
+{
+    //id，数据条目
+    Dictionary<int, TDatebase> _cache = new Dictionary<int, TDatebase>();
+
+    protected void LoadTable(string path)
+    {
+        var table = Resources.Load<TextAsset>(path);
+        var tableStream = new MemoryStream(table.bytes);
+        using (var reader = new StreamReader(tableStream, Encoding.GetEncoding("gb2312")))
+        {
+
+            reader.ReadLine();//跳过第一行
+
+            var lineStr = reader.ReadLine();
+            while (lineStr != null)
+            {
+                var itemStrArray = lineStr.Split(',');
+
+                var LoadDB = new TDatebase();
+                LoadDB.ID = int.Parse(itemStrArray[0]);
+
+                ReadDate(LoadDB, itemStrArray);
+
+                _cache[LoadDB.ID] = LoadDB;
+
+                lineStr = reader.ReadLine();
+            }
+        }
+    }
+
+    public TDatebase this[int index]
+    {
+        get
+        {
+            TDatebase db;
+            _cache.TryGetValue(index, out db);
+            return db;
+        }
+    }
+
+    public TDatebase GetData(int id)
+    {
+        TDatebase db;
+        _cache.TryGetValue(id, out db);
+        return db;
+    }
+
+    public Dictionary<int, TDatebase> GetAll()
+    {
+        return _cache;
+    }
+
+
+    protected virtual void ReadDate(TDatebase datebase,string[] itemStrArray)
+    {
+
+    }
+}
--- Assets/Framework/RoleTable.cs
+++ Assets/Framework/RoleTable.cs
@@ -1,41 +1,24 @@
-﻿//读表，暂未写完
-
-
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RoleDatebase
+public class RoleDatebase:BaseDatebase
 {
-    public int Id;
     public string Name;
     public string Des;
 }
 
 
-public class RoleTable : Singleton<RoleTable>
+public class RoleTable:ConfigTable<RoleDatebase,RoleTable>
 {
-    Dictionary<int, RoleDatebase> _cache = new Dictionary<int, RoleDatebase>();
-
     public RoleTable()
     {
-        
+        LoadTable("Config/table.csv");
     }
 
-    public RoleDatebase GetData(int id)
+    protected override void ReadDate(RoleDatebase datebase, string[] itemStrArray)
     {
-        RoleDatebase db;
-        _cache.TryGetValue(id, out db);
-        return db;
+        datebase.Name = itemStrArray[1];
     }
 
-    public RoleDatebase this[int index]
-    {
-        get
-        {
-            RoleDatebase db;
-            _cache.TryGetValue(index, out db);
-            return db;
-        }
-    }
 }
